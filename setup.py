6cf40534a47b25b2e8854d31afc9f304183f3c33
williamfzc <178894043@qq.com>
2019-05-16 17:48:05 +0800

    release: 0.3.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fitch',
-    version='0.3.2',
+    version='0.3.3',
     description='Android UI automation based on opencv',
     author='williamfzc',
     author_email='fengzc@vip.qq.com',
