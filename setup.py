82f94d47e5f5645390fae53815a1aee13dae9134
williamfzc <178894043@qq.com>
2019-05-23 09:56:27 +0800

    release: 0.3.4
    for supporting remote mode

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fitch',
-    version='0.3.3',
+    version='0.3.4',
     description='Android UI automation based on opencv',
     author='williamfzc',
     author_email='fengzc@vip.qq.com',
