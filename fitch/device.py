30d6690995d00f5cd3791f5e24e0e5bc4eeac9b4
williamfzc <178894043@qq.com>
2019-06-11 15:55:54 +0800

    feat(FDevice): replace pyatool with adbutils

--- fitch/device.py
+++ fitch/device.py
@@ -36,6 +36,7 @@ from fitch import detector
 
 from fastcap import MNCDevice
 from pyatool import PYAToolkit
+from adbutils import adb, AdbDevice
 
 
 class FDevice(object):
@@ -46,6 +47,7 @@ class FDevice(object):
         self.mnc: MNCDevice = None
         self.player: ActionPlayer = None
         self.toolkit: PYAToolkit = None
+        self.extras: AdbDevice = None
 
         self.start()
 
@@ -54,6 +56,7 @@ class FDevice(object):
         self.mnc = MNCDevice(self.device_id)
         self.player = ActionPlayer(self.device_id)
         self.toolkit = PYAToolkit(self.device_id)
+        self.extras = adb.device(serial=self.device_id)
 
         logger.debug('FDevice [{}] started'.format(self.device_id))
 
--- setup.py
+++ setup.py
@@ -14,5 +14,6 @@ setup(
         'fastcap',
         'pyatool',
         'loguru',
+        'adbutils'
     ]
 )
